b262e7c216bc5adcbaf5fa93d15fe0e87b8547b7
rkran <rkranjith03@gmail.com>
2024-10-17 00:00:13 +0530

    first commit

---
+++ src/app/(dashboard)/orders/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='w-full h-full px-6 overflow-y-auto pt-10 lg:pt-20 pb-10 md:px-8 lg:px-20 xl:px-[28rem] font-poppins'>
+
+        </div>
+    )
+}
+
+export default page
